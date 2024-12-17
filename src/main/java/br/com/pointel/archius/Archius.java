a96cfe73f8c02b399782cb9f9a5e4bf10773ae91
Éverton M. Vieira <emuvi@outlook.com.br>
2024-12-16 21:29:32 -0300

    adds basic start

---
+++ src/main/java/br/com/pointel/archius/Archius.java
@@ -0,0 +1,14 @@
+package br.com.pointel.archius;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Archius {
+
+    private static final Logger logger = LoggerFactory.getLogger(Archius.class);
+
+    public static void main(String[] args) {
+        logger.info("Started.");
+    }
+
+}
