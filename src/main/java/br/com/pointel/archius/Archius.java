952d6d034c895aab4b1df309e607a5e49df547f0
Éverton M. Vieira <emuvi@outlook.com.br>
2025-02-22 19:55:35 -0300

    feat: set default speed as available processors

--- src/main/java/br/com/pointel/archius/Archius.java
+++ src/main/java/br/com/pointel/archius/Archius.java
@@ -5,7 +5,8 @@ import br.com.pointel.jarch.mage.WizProps;
 public class Archius {
 
     public static final String ARCHIUS_SPEED = "ARCHIUS_SPEED";
-    public static final Integer DEFAULT_SPEED = WizProps.get(ARCHIUS_SPEED, 8);
+    public static final Integer DEFAULT_SPEED = WizProps.get(ARCHIUS_SPEED,
+                    Runtime.getRuntime().availableProcessors());
 
     public static void main(String[] args) {
         Desk.start(args);
